25ce966b80995cce6a33ff5cccb2c67883795bc2
Aditya <97244913+GrimBit1@users.noreply.github.com>
2022-11-14 17:24:05 +0000

    Initial commit

--- 06.Expression.js
+++ 06.Expression.js
@@ -4,11 +4,18 @@
 console.log(14)
 let a = 4
 let b =3
-console.log("The value of a + b =", a+b) // Expression for addition between number
-console.log("The value of a - b =", a-b)// Expression for substraction between number
-console.log("The value of a / b =", a/b) // Expression for dividation between number
-console.log("The value of a * b =", a*b) // Expression for multiplication between number
-console.log("The value of a^b =", a**b) // Expression for the a raise to the b
-console.log("The value of a%b =", a%b) // Expression for dividation between number and output the result
-console.log("The value of a / b =", a/b) // Expression for dividation between number
+// console.log("The value of a + b =", a+b) // Expression for addition between number
+// console.log("The value of a - b =", a-b)// Expression for substraction between number
+// console.log("The value of a / b =", a/b) // Expression for dividation between number
+// console.log("The value of a * b =", a*b) // Expression for multiplication between number
+// console.log("The value of a^b =", a**b) // Expression for the a raise to the b
+// console.log("The value of a%b =", a%b) // Expression for dividation between number and output the result
+// console.log("The value of a / b =", a/b) // Expression for dividation between number
 
+// Increment operator and decrement operator
+console.log("The value of a with increment after printing", a++)
+console.log("The value of a with increment before printing",++a)
+console.log("The value of a with decrement after printing", a--)
+console.log("The value of a with decrement before printing", --a)
+console.log("The value of a ", a)
+console.log(a)
