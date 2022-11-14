7ec594bc1a1f384aad1f70eb8522dbe53f13136c
Aditya <97244913+GrimBit1@users.noreply.github.com>
2022-11-14 05:05:29 +0000

    Initial commit

--- 06.Expression.js
+++ 06.Expression.js
@@ -4,8 +4,11 @@
 console.log(14)
 let a = 4
 let b =3
-console.log("The value of a + b =", a+b)
-console.log("The value of a - b =", a-b)
-console.log("The value of a / b =", a/b)
-console.log("The value of a * b =", a*b)
+console.log("The value of a + b =", a+b) // Expression for addition between number
+console.log("The value of a - b =", a-b)// Expression for substraction between number
+console.log("The value of a / b =", a/b) // Expression for dividation between number
+console.log("The value of a * b =", a*b) // Expression for multiplication between number
+console.log("The value of a^b =", a**b) // Expression for the a raise to the b
+console.log("The value of a%b =", a%b) // Expression for dividation between number and output the result
+console.log("The value of a / b =", a/b) // Expression for dividation between number
 
