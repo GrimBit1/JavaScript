c7e18c13c218ae5d1195730179a69342a9ec6116
Aditya <adityanandwana19@gmail.com>
2022-11-30 21:23:47 +0530

    Initial commit

--- 07.Conditional_Expressions.js
+++ 07.Conditional_Expressions.js
@@ -1,8 +1,14 @@
-alert("HI")
-let a = prompt("Hi please tell your age 🥺")
+const prompt=require("prompt-sync")({sigint:true});
+
+// alert("HI")
+let a = prompt("Hi please tell your age 🥺 ")
 a = Number.parseInt(a)
 console.log(typeof a)
+console.log(a)
 if (a>0) {
   console.log(`Age is valid`);
    
+}
+else{
+  console.log("Hi")
 }
