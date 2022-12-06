805b3b5fa9c93c87bd95d4ed65906dbc6ff1f396
Aditya <97244913+GrimBit1@users.noreply.github.com>
2022-12-06 16:59:49 +0000

    Initial commit

---
+++ 14.Strings.js
@@ -0,0 +1,19 @@
+// Strings are used to store or manipulate text
+// We can make strings in 3 ways
+let i ="Aditya" // With Double qoute ""
+let me ='Aditya' // With Single qoute ''
+let a = `Aditya`// With Chapstick
+//we can also use variable in a string with chapstick 
+a = `${me}`
+console.log(a);
+
+// Escape sequence characters 
+console.log('Hello \' World'); // \ works as escape character which breaks a string define
+
+console.log(`Hi im \nAditya`); // \n breaks the sequence and put that in new line
+
+console.log(`Hi im \tAditya`);
+// \t put the tab in the sentence 
+
+console.log(`Hi im \rAditya`);
+// \r doesn't print before it
--- primeno.js
+++ primeno.js
@@ -2,10 +2,10 @@ const prompt=require("prompt-sync")({sigint:true});
 let a = parseInt(prompt("Enter A number "))
 let b = parseInt(prompt("Enter A number "))
 for(a=a ;a<=b;a++){
-    for(let i = a;i<=b;)
+    for(let i = a;i<=b;){
     let c = a%i
     console.log(a)
-    console.log(i)
+    console.log(i)}
     // if(c==0){
     //   console.log(a)
     // }
