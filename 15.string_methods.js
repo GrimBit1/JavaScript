c68505309824c2613b337e81f7955901aaf8ff2e
Aditya <97244913+GrimBit1@users.noreply.github.com>
2022-12-07 05:01:31 +0000

    Initial commit

---
+++ 15.string_methods.js
@@ -0,0 +1,10 @@
+let name = "Aditya"
+console.log(name.length); // Gives the no of string character
+
+console.log(name.toUpperCase());
+// U got it by name but that's a function not property 
+console.log(name.toLowerCase())
+
+console.log(name.slice(2,4))
+//This will print 2,3 but not 4
+// Starts from 2 to 4
