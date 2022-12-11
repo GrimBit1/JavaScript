5d7bc607a7f636a04e05cfcd17dbf1215fa9df59
Aditya <adityanandwana19@gmail.com>
2022-12-11 23:15:55 +0530

    Initial commit

---
+++ 16.Practice_Set4.js
@@ -0,0 +1,28 @@
+// Problem no 1
+console.log("adi\"".length)
+
+/*
+// Problem no 2
+// includes() function returns true or false
+let a = "Aditya is a good boy"
+let b = 'boy'
+let b1 = ""
+let c = a.includes(b)
+let d = false
+
+console.log(c)
+console.log(Boolean(b1))
+console.log(`The word ${b} ${ d ? "is" : "isnt"  } in the sentence`) */
+
+// problem no 3
+let a = "Aditya"
+
+a = a.toLowerCase()
+console.log(a)
+
+let capsDown = (a) =>{
+    a = a.toLowerCase()
+    console.log(a)
+}
+capsDown("Hi");
+
