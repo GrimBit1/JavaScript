45475f87f70296f2ffe69659d99a395a85a4adf0
Aditya <97244913+GrimBit1@users.noreply.github.com>
2022-12-11 16:47:39 +0000

    Initial commit

--- 15.string_methods.js
+++ 15.string_methods.js
@@ -8,3 +8,25 @@ console.log(name.toLowerCase())
 console.log(name.slice(2,4))
 //This will print 2,3 but not 4
 // Starts from 2 to 4
+
+// one more method to slice the string
+console.log(name.slice(2))// It will slice from 2 to end
+
+name ="Aditya"
+// method to replace character in strings
+console.log(name.replace("a","i")) // (But only first it founds in the string)
+
+// Concat function it will join two or more strings in a string 
+let friend = "Shiv"
+console.log(name.concat(" , Hi This is my friend ", friend));
+name = "  Aditya  "
+//Trim function to trim string to trim whitespace 
+console.log(name.trim());
+
+// Index of function gives index of that string
+let fr = "Kahna"
+
+
+for (i=0;i <= fr.length; i++){
+  console.log(fr[i]);
+  }
