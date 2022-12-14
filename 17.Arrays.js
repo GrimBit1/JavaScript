bbb1873bcec509712236b4e16bed02c1267e70b1
Aditya <adityanandwana19@gmail.com>
2022-12-14 22:41:55 +0530

    Initial commit

---
+++ 17.Arrays.js
@@ -0,0 +1,4 @@
+// Arrays is a collection of items
+let a = [19 , 20 , 2 , true ,"Hi"] // The array is defined between square brackets 
+// Array can store any type of data 
+console.log(a[2]) // a[2] is used to access the value of array in 2nd index
