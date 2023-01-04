a50b1d6195829623efad5991c0a0e7d5db8d9a71
Aditya Nandwana <adityanandwana19@gmail.com>
2023-01-04 15:58:46 +0530

    Initial commit

---
+++ 21.map_fillter.js
@@ -0,0 +1,23 @@
+// // Map array method is like forEach but creates new array with new element
+//  let arr = [23,31,14,15]
+//  console.log(arr)
+//  let a = arr.map((a,b,c)=>{
+//     console.log(a,b,c)
+//     return a**2
+//  })
+//  console.log(a)
+
+// // Filter method fillter the array 
+// let smal = (a)=>{
+//     return a<5
+// }
+// let arr2 = [1,2,3,4,5,6,7,8,9,10]
+// let a2 = arr2.filter(smal)
+// console.log(a2)
+
+// Reduce array method returns a value
+let c = [1,2,3,4,5,6]
+let c2 = c.reduce((a,b)=>{
+    return a*b
+})
+console.log(c2)
--- factorial.js
+++ factorial.js
@@ -1,7 +1,10 @@
 const prompt=require("prompt-sync")({sigint:true});
 
-let a = parseInt(prompt("Enter A number"))
-for(let i = (a-1);i>0;i--){
-   a*=i
-}
-console.log(a)
+// let a = parseInt(prompt("Enter A number"))
+// for(let i = (a-1);i>0;i--){
+//    a*=i
+// }
+// console.log(a)
+
+// New Method for factorial
+let a = parseInt(prompt("Enter the number to find the factorial"))
