f21dd6687aec1a3ee7083005b2371ec9bf8373ae
Aditya <97244913+GrimBit1@users.noreply.github.com>
2023-01-07 09:34:17 +0000

    Initial commit

---
+++ 23.Guess.js
@@ -0,0 +1,22 @@
+// Question 
+let a = parseInt(Math.random()*101)
+console.log(a);
+let arr = []
+
+while (true) {
+let b = parseInt(prompt("Guess the number"))
+  if (a==b){
+    console.log(`You are correct`);
+    break
+  }
+else if (a>b){
+console.log(`The number is smaller than the correct one`);}
+else {
+console.log(`The number is bigger than the correct one`);}
+  arr.push(b)
+  
+
+   ;
+}
+let i = 100 -arr.length
+console.log(`Your score is ${i} %`);
