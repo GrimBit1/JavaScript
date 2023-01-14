ad241c3961ddf9fdd2895f708bfc04f4ecff4c14
Aditya Nandwana <adityanandwana19@gmail.com>
2023-01-14 09:14:31 +0530

    Initial commit

---
+++ 26.confirm_alert.js/script.js
@@ -0,0 +1,15 @@
+console.log("HI")
+
+alert("Hi enter ht eejerf") // Alert alerts in the browser
+
+let a = prompt("Hi give a")// Asks input from user and stored in variable as string
+console.time("Hi")
+
+
+console.timeEnd("Hi")
+console.log(a)
+
+let b = confirm("do you want to write in document") // confirm returns boolean value as true or false
+if (b){
+    document.write("a")
+}
