aa9a29f498aab0de4dbab0b1660e6715434a14e6
Aditya Nandwana <adityanandwana19@gmail.com>
2023-01-15 10:26:43 +0530

    Initial commit

---
+++ 27.Dom,Bom.js/script.js
@@ -0,0 +1,12 @@
+
+
+// Window is a global object and it has all the features/function of javascript
+
+window.console.log("HI") // like this we dont have to use the window object for function because it is a global object
+console.log("HI") // Both work is same
+
+console.log(window) // it will show all the features of window object 
+
+// DOM is document object model 
+// the whole html page is made an object as dom
+console.table(document) // this will print document object
