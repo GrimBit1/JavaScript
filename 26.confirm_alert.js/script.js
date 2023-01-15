e7b5d269ad414d81227cec64b9d28b963f394944
Aditya Nandwana <adityanandwana19@gmail.com>
2023-01-15 09:47:56 +0530

    Initial commit

--- 26.confirm_alert.js/script.js
+++ 26.confirm_alert.js/script.js
@@ -1,20 +1,21 @@
 console.log("HI")
 
-alert("Hi enter ht eejerf") // Alert alerts in the browser
+// alert("Hi enter ht eejerf") // Alert alerts in the browser
 
-let a = prompt("Hi give a")// Asks input from user and stored in variable as string
-console.time("Hi")
+// let a = prompt("Hi give a")// Asks input from user and stored in variable as string
+// console.time("Hi")
 
 
-console.timeEnd("Hi")
-console.log(a)
+// console.timeEnd("Hi")
+// console.log(a)
 
-let b = confirm("do you want to write in document") // confirm returns boolean value as true or false
-if (b){
-    document.write("<div style=\"background-color: aqua;\"> Hi</div>") // Document.write write in the html
-}
+// let b = confirm("do you want to write in document") // confirm returns boolean value as true or false
+// if (b){
+//     document.write("<div style=\"background-color: aqua;\"> Hi</div>") // Document.write write in the html
+// }
 let changecss = () =>{
-    let a = document.getElementsByTagName("link")[1]
-    document.write(a)
-    a.href = "style2.css"
+    document.getElementsByTagName('link')[1].href = 'style2.css'
+}
+let changecss2 = () =>{
+    document.getElementsByTagName('link')[1].href = 'style.css'
 }
