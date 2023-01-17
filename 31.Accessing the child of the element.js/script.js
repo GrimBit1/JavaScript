b8893aa03948d834fb3114351355c4f58a7a9389
Aditya Nandwana <adityanandwana19@gmail.com>
2023-01-17 17:25:55 +0530

    Initial commit

---
+++ 31.Accessing the child of the element.js/script.js
@@ -0,0 +1,7 @@
+console.log("hi");
+console.log(document.body.firstChild); // this will  all the last child node in console
+console.log(document.body.lastChild); // this will  all the last child node in console
+console.log(document.body.childNodes); // this will give all child node in console
+// childNodes creates a collection that looks like an array but not still i can get child by using the index system
+console.log(document.body.childNodes[0])
+// document.write(document.body.firstChild)
