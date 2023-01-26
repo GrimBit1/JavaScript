402814eca42674dd09c854aa7f6f8f36169de1bf
Aditya Nandwana <you@example.com>
2023-01-26 21:08:14 +0530

    Initial commit

---
+++ 32.Parents_and_sibling_of_an_element.js/script.js
@@ -0,0 +1,7 @@
+console.log("hi")
+console.log(document.body.childNodes)
+// Dom collection is read only element 
+// It automatically refreshes
+
+// I can access next sibling with this property
+console.log(document.head.nextSibling)
