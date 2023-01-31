244acfb00ebcda71375605fb5943bbcef5706cc8
Aditya Nandwana <you@example.com>
2023-01-31 23:03:53 +0530

    Initial commit

--- 36.Matches_element.js/script.js
+++ 36.Matches_element.js/script.js
@@ -1,4 +1,18 @@
-// Matches
+// Matches 
 let id1 = document.getElementById("id1")
 console.log(id1)
 
+console.log(id1.matches("#id1")) //return true and false if the css selector matches
+console.log(id1.matches(".box"))
+
+// Javascript is so smart if i just use the name of variable as id given to element it will return the element
+console.log(id2)
+
+// Closest just gives the element if his parent has that the css selector
+console.log(id1.closest('.hi'))
+
+// Contains return 
+let body = document.getElementById("body")
+let hi = document.getElementById("hi")
+console.log(body)
+console.log(body.contains(hi)) // this will only give return any if output that is already declared in a variable 
